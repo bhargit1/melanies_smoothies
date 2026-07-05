173e38f569a9811ccd9dcd4f70fb8b8e4f347e5a
bhargit1 <spidyyrock@gmail.com>
2026-07-04 20:33:51 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -47,6 +48,6 @@ if ingredients_list :
         
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
 
-import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")
 st.text(smoothiefroot_response)
