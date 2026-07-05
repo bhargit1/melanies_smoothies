b6dcd270ab793a7b40347871f600246ab9a7e071
bhargit1 <spidyyrock@gmail.com>
2026-07-04 20:38:57 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list :
-   
+
     ingredients_string=''
 
     for fruit_chosen in ingredients_list:
@@ -42,12 +42,10 @@ if ingredients_list :
     #st.write(my_insert_stmt)
     #st.stop()
     time_to_insert=st.button('Submit Order')
-    
+    smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")
+    st.text(smoothiefroot_response)   
+  
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
-
-
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")
-st.text(smoothiefroot_response)
